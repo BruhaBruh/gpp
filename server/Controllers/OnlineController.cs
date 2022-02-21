c3537eb780ac617591628cc93a061efcc780274e
BruhaBruh <drugsho.jaker@gmail.com>
2022-02-21 09:35:46 +0500

    feat: add online open api

---
+++ server/Controllers/OnlineController.cs
@@ -0,0 +1,23 @@
+﻿using GPPlanetGQL.Services;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPPlanetGQL.Controllers
+{
+    public class OnlineController : Controller
+    {
+        private LimeService _limeService;
+
+        public OnlineController(IConfiguration config, [Service] LimeService _limeService)
+        {
+            _limeService = _limeService;
+        }
+
+        // GET: OnlineController
+        public ActionResult Index()
+        {
+            return Ok(_limeService.GetOnline().Result);
+        }
+    }
+}
